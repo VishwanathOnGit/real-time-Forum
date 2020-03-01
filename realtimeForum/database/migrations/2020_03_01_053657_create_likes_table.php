a6214b7b2d302efca50e74b0e9452b4f3e1bd27e
Vishwanath Putcha <putchavishwanath@gmail.com>
2020-03-01 18:02:43 +0530

    Relationships

--- realtimeForum/database/migrations/2020_03_01_053657_create_likes_table.php
+++ realtimeForum/database/migrations/2020_03_01_053657_create_likes_table.php
@@ -15,8 +15,8 @@ class CreateLikesTable extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->increments('id');
-            $table->interger('reply_id');
-            $table->interger('user_id');
+            $table->integer('reply_id');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
